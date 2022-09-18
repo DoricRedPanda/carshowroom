f6626ef57944acf29a3424e3d2201e04161610fe
DoricRedPanda <azatoth@ro.ru>
2022-09-18 04:38:21 +0300

    Initial commit. Stage1.

---
+++ db/create.sql
@@ -0,0 +1,56 @@
+DROP DATABASE carshow;
+
+CREATE DATABASE carshow;
+
+\connect carshow
+
+CREATE TABLE "model" (
+  "model_id" serial PRIMARY KEY,
+  "name" text NOT NULL,
+  "year" int NOT NULL,
+  "make" text NOT NULL
+);
+
+CREATE TABLE "car" (
+  "vin" serial PRIMARY KEY,
+  "model_id" int NOT NULL,
+  "registration_plate" text,
+  "price" int NOT NULL,
+  "kilometers" real,
+  "service_date" date,
+  "displacement" int,
+  "power" real,
+  "top_speed" real,
+  "seat_count" int,
+  "transmission_type" text,
+  "devices" text,
+  "color" text,
+  "saloon" text,
+  CONSTRAINT "FK_car.model_id"
+    FOREIGN KEY ("model_id")
+      REFERENCES "model"("model_id")
+);
+
+CREATE TABLE "client" (
+  "client_id" serial PRIMARY KEY,
+  "name" text NOT NULL,
+  "phone" text,
+  "email" text,
+  "address" text
+);
+
+CREATE TABLE "contract" (
+  "contract_id" serial PRIMARY KEY,
+  "client_id" int NOT NULL,
+  "vin" int NOT NULL,
+  "date" date NOT NULL,
+  "test_drive" bool NOT NULL,
+  "status" smallint NOT NULL,
+  CONSTRAINT "FK_contract.vin"
+    FOREIGN KEY ("vin")
+      REFERENCES "car"("vin"),
+  CONSTRAINT "FK_contract.client_id"
+    FOREIGN KEY ("client_id")
+      REFERENCES "client"("client_id")
+);
+
---
+++ db/insert.sql
@@ -0,0 +1,34 @@
+\connect carshow
+
+INSERT INTO model
+	("name", "year", "make") VALUES
+	('Maxima', 2022, 'Nissan'),
+        ('Qashqai', 2022, 'Nissan'),
+        ('Rio', 2022, 'Kia'),
+        ('Optima', 2022, 'Kia'),
+        ('Forte', 2022, 'Kia');
+
+INSERT INTO client
+	("name", "address", "phone", "email") VALUES
+	('Luke Smith', 'memes', null, 'luke@lukesmith.xyz'),
+        ('John Smith', 'USA', '+77777777777', null),
+        ('Richard Stallman', 'USA', null, 'rms@gnu.org'),
+        ('Linus Torvalds', 'USA', null, null),
+        ('Andrey Stolyarov', 'Russia, Moscow', null, null),
+        ('Andrey Stolyarov', 'Russia', null, null);
+
+INSERT INTO car
+	("model_id", "registration_plate", "price", "kilometers", "service_date", "displacement", "power", "top_speed", "seat_count", "transmission_type", "devices", "color", "saloon") VALUES
+	(1, 'c111mk', 300000, 0, '2017-01-12', 1995, 103, 203, 4, 'AT', 'radio', 'black', 'high'),
+	(1, 'c111ab', 400000, 100, '2018-01-12', 1995, 103, 203, 4, 'AT', 'radio,GPS,camera', 'white', 'high'),
+	(2, 'c111cd', 350000, 500, '2019-01-12', 1332, 103, 197, 4, 'AT', 'radio', 'black', 'high');
+
+
+INSERT INTO contract
+	("client_id", "vin", "date", "test_drive", "status") VALUES
+	(1, 1, '2022-01-01', true, 1),
+	(2, 1, '2022-01-02', true, 2),
+	(3, 2, '2021-01-01', true, 3),
+	(1, 3, '2022-01-01', true, 4),
+	(1, 2, '2021-05-01', true, 1),
+	(4, 1, '2021-01-01', true, 1);
